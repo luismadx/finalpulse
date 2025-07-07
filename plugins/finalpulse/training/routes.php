3f9d6e63bad1ab26be6467f4b01cfc1b61876cee
luismadx <83168611+luismadx@users.noreply.github.com>
2025-07-07 20:54:02 +0100

    Delete plugins directory

--- plugins/finalpulse/training/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-use FinalPulse\Training\Classes\QrCodeGenerator;
-
-Route::get('training/qr/{texto}', QrCodeGenerator::class)
-     ->where('texto', '.*');   // opcional: aceita barras, %20, etc.
